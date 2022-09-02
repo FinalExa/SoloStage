3b8c347700372e76435bd539c57e977c57c85986
Alex Silvestri <alexsilvestri.16@gmail.com>
2022-09-02 21:01:01 +0200

    Import

---
+++ Assets/Player/Scripts/PCReferences.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PCReferences : MonoBehaviour
+{
+    public PCData pcData;
+    public Attack[] attack;
+    [HideInInspector] public Camera cam;
+    [HideInInspector] public Inputs inputs;
+    [HideInInspector] public Rigidbody rb;
+    [HideInInspector] public PCCombo pcCombo;
+    [HideInInspector] public PlayableDirector combos;
+
+    private void Awake()
+    {
+        cam = FindObjectOfType<Camera>();
+        inputs = this.gameObject.GetComponent<Inputs>();
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        pcCombo = this.gameObject.GetComponent<PCCombo>();
+    }
+}
